fdb9f2c2110d1642d27e473ae9e9b3c46b30aee3
nezhataghy <nezhataghy@gmail.com>
2024-02-25 23:12:36 +0000

    Classmapper implemented

--- models/Food.py
+++ models/Food.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """This module defines a Food class that will represent the Food table in the database."""
-from models.Basemodel import BaseModel
+from models.Basemodel import BaseModel, Base as ClassMapper
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Food(BaseModel):
-    """"Food class defines the food table in the database"""
+class Food(BaseModel, ClassMapper):
+    """Food class defines the food table in the database. Inherits from BaseModel
+    BaseModel: Representing common attributes and methods for Food class."""
 
-    # Table Columns
-    name = ""
-    category = ""
-    image = b""
+    __tablename__ = 'Food'
+
+    # Define the columns of the table. Each class attribute represents a column in the tables.
+    name = Column(String(200), nullable=False, unique=True)
+    category = Column(String(200), nullable=False)
+    image = Column(String(200), default='No image')
+    ingredients = relationship("Ingredient", secondary='Food_Ingredients', 
+                               back_populates='food', viewonly=False)
+    recipe = relationship("Recipe", cascade='all, delete-orphan', backref='food')
     
-    def __init__(self, name, category, image):
-        super().__init__()
-        self.name = name
-        self.category = category
-        self.image = image
-        """Temporary constructor to simulate the data"""
